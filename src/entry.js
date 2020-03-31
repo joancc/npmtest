2ae17f5df90acda9b9ec67c6135a6880b531897e
Joan Cejudo <joancc@gmail.com>
2020-03-31 17:09:35 -0400

    UPDATE

--- src/entry.js
+++ src/entry.js
@@ -1,1 +1,26 @@
-import component from "./SimpleAlert.vue";
+import component from "./components/SimpleAlert.vue";
+
+function install(Vue) {
+  if (install.installed) return;
+  install.installed = true;
+  Vue.component("SimpleAlert", component);
+}
+
+const plugin = {
+  install
+};
+
+let GlobalVue = null;
+if (typeof window !== "undefined") {
+  GlobalVue = window.Vue;
+} else if (typeof global !== "undefined") {
+  GlobalVue = global.vue;
+}
+
+if (GlobalVue) {
+  GlobalVue.use(plugin);
+}
+
+component.install = install;
+
+export { component };
